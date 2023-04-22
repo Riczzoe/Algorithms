0cf3999c416c94efae2df689c4c9b98c8b7bc356
FrankJohn <86246881+JohnFrankz@users.noreply.github.com>
2023-04-22 20:39:55 +0800

    Init assignments collinear

---
+++ assignment/collinear/src/BruteCollinearPoints.java
@@ -0,0 +1,13 @@
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+
+    }
+
+    public LineSegment[] segments() {
+
+    }
+}
---
+++ assignment/collinear/src/FastCollinearPoints.java
@@ -0,0 +1,13 @@
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    public           int numberOfSegments() {
+
+    }
+
+    public LineSegment[] segments() {
+
+    }
+}
