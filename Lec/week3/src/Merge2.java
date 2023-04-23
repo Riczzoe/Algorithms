66682312049eea84103134e4096fe243d24c7df2
FrankJohn <86246881+JohnFrankz@users.noreply.github.com>
2023-04-23 20:48:32 +0800

    Fix bug in Merge.java

--- Lec/week3/src/Merge2.java
+++ Lec/week3/src/Merge2.java
@@ -2,10 +2,13 @@ public class Merge2 extends SortUtil {
     public static Comparable[] aux;
 
     public static void merge(Comparable[] a, int lo, int mid, int hi) {
-        System.out.println("merge(a, " + lo + ", " + mid + ", " + hi + ")");
+        // System.out.println("merge(a, " + lo + ", " + mid + ", " + hi + ")");
+        System.out.println("merge(" + lo + ", " + mid + ", " + hi + ")");
         int i = lo; int j = mid + 1;
-        if (hi + 1 - lo >= 0) {
-            System.arraycopy(a, lo, aux, lo, hi + 1 - lo);
+        // System.out.println(hi - lo + 1);
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
         }
 
         for (int k = lo; k <= hi; k++) {
@@ -42,20 +45,22 @@ public class Merge2 extends SortUtil {
         int N = a.length;
         aux = new Comparable[N];
 
-        for (int size = 2; size <= N; size *= 2) {
-            for (int index = 0; index <= N - size; index += size) {
+        for (int size = 2; size <= N * 2; size *= 2) {
+            for (int index = 0; index < N - size / 2; index += size) {
                 int hi = index  + size - 1;
                 hi = hi < N ? hi : N - 1;
-                merge(a, index, (index + hi) / 2, hi);
+                merge(a, index, index + size / 2 - 1, hi);
             }
         }
-        show(a);
-        merge(a, 0, 7, 15);
+        // show(a);
+        // merge(a, 0, N / 2, N - 1);
     }
 
     public static void main(String[] args) {
         Character[] a = new Character[] {
-                'M', 'E', 'R', 'G', 'E', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'
+                'M', 'E', 'R', 'G', 'E', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E',
+                'M', 'E', 'R', 'G', 'E', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E',
+                'M', 'E', 'R', 'G', 'E', 'S', 'O'
         };
         System.out.println("Before Sort:");
         show(a);
